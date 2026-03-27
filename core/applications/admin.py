32154dfb3cd6cd869b4f31791c51e9b022d785a9
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-27 20:28:55 +0800

    ULIT NABUWISIT AKO

--- core/applications/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import InternshipApplication
-
-# Register your models here.
-
-@admin.register(InternshipApplication)
-class InternshipApplicationAdmin(admin.ModelAdmin):
-    list_display = ('student', 'company', 'status', 'applied_on')
-    list_filter = ('status',)
--- core/applications/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppplicationsConfig(AppConfig):
-    name = 'applications'
