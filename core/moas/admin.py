62cb0d6ca94e8d3351dbfdd456899b8593b75acf
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-27 22:02:32 +0800

    edited moas app admin.py

--- core/moas/admin.py
+++ core/moas/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import MOA, MOARequest
 
 # Register your models here.
+
+@admin.register(MOA)
+class MOAAdmin(admin.ModelAdmin):
+    list_display = ('company', 'date_signed', 'expiration_date', 'is_active')
+    list_filter = ('is_active', 'date_signed')
+    search_fields = ('company__name',)
+
+@admin.register(MOARequest)
+class MOARequestAdmin(admin.ModelAdmin):
+    list_display = ('student', 'target_company_name', 'status', 'date_requested')
+    list_filter = ('status', 'date_requested')
+    search_fields = ('target_company_name', 'student__username')
