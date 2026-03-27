7d278f5ee0d4ba26da98e96384513909c7f73243
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-27 22:06:04 +0800

    edited doccumnets admin.py

--- core/documents/admin.py
+++ core/documents/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import RequirementDocument
 
 # Register your models here.
+
+@admin.register(RequirementDocument)
+class RequirementDocumentAdmin(admin.ModelAdmin):
+    list_display = ('title', 'specific_company')
+    list_filter = ('specific_company',)
+    search_fields = ('title', 'description')
