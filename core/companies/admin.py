e91ae7bfeac6136fddb31bc6fc764624ba079c69
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-27 22:01:29 +0800

    edited companies app admin.py

--- core/companies/admin.py
+++ core/companies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tag, Company
 
 # Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'contact_email', 'website', 'is_active')
+    list_filter = ('is_active', 'tags')
+    search_fields = ('name', 'description')
