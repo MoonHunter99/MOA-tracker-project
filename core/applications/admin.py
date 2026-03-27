822d7c1e890fce1df9786336018cb4fd0d939fa2
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-27 22:04:04 +0800

    edited applications app admin.py

--- core/applications/admin.py
+++ core/applications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import InternshipApplication
 
 # Register your models here.
+
+@admin.register(InternshipApplication)
+class InternshipApplicationAdmin(admin.ModelAdmin):
+    list_display = ('student', 'company', 'status', 'application_date')
+    list_filter = ('status', 'application_date')
+    search_fields = ('student__username', 'company__name')
