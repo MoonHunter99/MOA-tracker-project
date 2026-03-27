8364f6be6fec0bef4af027468463e8b33b945a37
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-28 01:12:08 +0800

    accounts app views.py is updated

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Automatically log the user in after they register
+            login(request, user)
+            return redirect('companies:dashboard')
+    else:
+        form = UserCreationForm()
+        
+    return render(request, 'registration/register.html', {'form': form})
