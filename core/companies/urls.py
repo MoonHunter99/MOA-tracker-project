5972ae4d704cc6b8bef1189313ebcbeae3d43765
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-27 23:04:46 +0800

    updated the views.py of comapanies app

--- core/companies/urls.py
+++ core/companies/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'companies'
+
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('<int:pk>/', views.company_detail, name='company_detail'),
 ]
