02f30531b9c33dab7cc30802c9237a5b05941ce4
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-27 20:48:15 +0800

    added models in the app caompanies

--- core/companies/models.py
+++ core/companies/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    # Examples: 'Software', 'Hardware', 'Embedded Systems', 'Flutter', 'DSP'
+    name = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.name
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    contact_email = models.EmailField(blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    tags = models.ManyToManyField(Tag, related_name='companies')
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "Companies"
+
+    def __str__(self):
+        return self.name
