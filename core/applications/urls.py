1352f5d34e07796a2ee9bf88399110a65f8bf89e
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-28 11:06:17 +0800

    updated the urls.py of the applications app

--- core/applications/urls.py
+++ core/applications/urls.py
@@ -5,4 +5,5 @@ app_name = 'applications'
 
 urlpatterns = [
     path('apply/<int:company_id>/', views.apply_to_company, name='apply'),
+    path('my-applications/', views.my_applications, name='my_applications'),
 ]
