da63391eb20e7e2263ea217875a30017f72d961d
MoonHunter99 <128887327+MoonHunter99@users.noreply.github.com>
2026-03-28 11:14:36 +0800

    uppdated views.py of the moas app

--- core/moas/views.py
+++ core/moas/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import MOARequestForm
 
 # Create your views here.
+
+@login_required
+def request_moa(request):
+    if request.method == 'POST':
+        form = MOARequestForm(request.POST)
+        if form.is_valid():
+            moa_request = form.save(commit=False)
+            moa_request.student = request.user
+            moa_request.status = 'pending'
+            moa_request.save()
+            # Redirect back to the dashboard after successfully submitting
+            return redirect('companies:dashboard') 
+    else:
+        form = MOARequestForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'moas/request_moa.html', context)
